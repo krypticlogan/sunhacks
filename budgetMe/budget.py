21796adf8260a7fc66fca1ca7f75d849b54f4cd1
Logan <logan.zchjones@gmail.com>
2024-09-28 15:32:24 -0700

    more templating

--- budgetMe/budget.py
+++ budgetMe/budget.py
@@ -3,5 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder='templateFiles',static_folder='static')
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template('index.html')
+
+@app.route("/analysis")
+def breakdown():
+    return render_template('analysis.html')
+
