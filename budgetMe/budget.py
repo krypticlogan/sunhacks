028332a4dfc95131707f7836f9c14656f847bca0
Logan <logan.zchjones@gmail.com>
2024-09-29 03:49:33 -0700

    teller linked

--- budgetMe/budget.py
+++ budgetMe/budget.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, jsonify, redirect, request
 import requests
+# from json impo
 app = Flask(__name__, template_folder='templateFiles',static_folder='static')
-
+access_token = None
 @app.route("/")
 def home():
     return render_template('index.html')
 
 @app.route("/breakdown")
 def breakdown():
-    return render_template('myBreakdown.html')
+    print(access_token)
+    if access_token:
+        return render_template('myBreakdown.html')
+    else:
+        return render_template('login.html')
 
 @app.route("/advice")
 def advice():
@@ -21,6 +26,19 @@ def login():
 @app.route("/teller")
 def teller():
     return render_template('teller.html')
+
+@app.route('/save-token', methods=['POST'])
+def save_token():
+    data = request.json
+    global access_token
+    access_token = data.get('accessToken')
+
+    if not access_token:
+        return jsonify({'error': 'Access token missing'}), 400
+
+    # Save token (for demonstration)
+    print(f"Access Token: {access_token}")
+    return render_template('myBreakdown.html')
 # @app.route("/advice")
 # def breakdown():
 #     return render_template('advice.html')
